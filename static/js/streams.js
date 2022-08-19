336454afba0746103d8eca5fe451756df9129a16
Shahzod95 <aktamovsh6975@gmail.com>
2022-08-19 19:40:28 +0500

    set stream for js file

---
+++ static/js/streams.js
@@ -0,0 +1,5 @@
+const APP_ID = '4538ac1c2b484378a0f19de6fa45d80b'
+const CHANNEL = 'main'
+const TOKEN = '007eJxTYNCYci8gnNF/5qlq3UOT1FbKTVtc1+Zz9Midss72VS2HLXUVGExMjS0Skw2TjZJMLEyMzS0SDdIMLVNSzdISTUxTLAySLs3/nxT0kSH5ctE/FkYGCATxWRhyEzPzGBgAiioiuw=='
+
+console.log('streamjs')
