6b7dc5e6852c9d077195bfe305db4eef83ce7f2c
Shahzod95 <aktamovsh6975@gmail.com>
2022-08-19 20:13:04 +0500

    Kompyuter kamerasi bilan bog'landi

--- static/js/streams.js
+++ static/js/streams.js
@@ -1,5 +1,28 @@
 const APP_ID = '4538ac1c2b484378a0f19de6fa45d80b'
 const CHANNEL = 'main'
 const TOKEN = '007eJxTYNCYci8gnNF/5qlq3UOT1FbKTVtc1+Zz9Midss72VS2HLXUVGExMjS0Skw2TjZJMLEyMzS0SDdIMLVNSzdISTUxTLAySLs3/nxT0kSH5ctE/FkYGCATxWRhyEzPzGBgAiioiuw=='
+let UID
+const client = AgoraRTC.createClient()
 
-console.log('streamjs')
+let localTracks = [] 
+let remoteUsers = {}
+
+let joinAndDisplayLocalStream = async () => {
+    UID = await client.join(APP_ID, CHANNEL, TOKEN, null)
+    
+    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
+    let player = ` <div class="video-container" id="user-container-${UID}">
+                    <div class="username-wrapper"><span class="user-name">My Name</span></div>
+                    <div class="video-player" id="user-${UID}"></div>
+                </div>`
+
+    document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+
+    localTracks[1].play(`user-${UID}`)
+
+    await client.publish([localTracks[0], localTracks[1]])
+
+}
+
+joinAndDisplayLocalStream()
+
